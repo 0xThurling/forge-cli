fbfdbfc774fff943bfff6687969351e0c591467c
Jacques Thurling <jthurling78@gmail.com>
2026-03-23 10:40:26 +0200

    update

---
+++ CMakeGeneration/Sections/LinkingSection.cs
@@ -0,0 +1,15 @@
+﻿using forge.Models;
+
+namespace forge.CMakeGeneration.Sections;
+
+public class LinkingSection : CMakeSectionBase
+{
+    public override string Name => throw new NotImplementedException();
+
+    public override int Priority => throw new NotImplementedException();
+
+    public override string Generate(ProjectConfig config)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CMakeGeneration/Sections/StandardSection.cs
@@ -0,0 +1,15 @@
+﻿using forge.Models;
+
+namespace forge.CMakeGeneration.Sections;
+
+public class StandardSection : CMakeSectionBase
+{
+    public override string Name => throw new NotImplementedException();
+
+    public override int Priority => throw new NotImplementedException();
+
+    public override string Generate(ProjectConfig config)
+    {
+        throw new NotImplementedException();
+    }
+}
