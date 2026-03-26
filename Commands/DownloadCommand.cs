b8e1d211c0db0e0bf4b0cb7d95bbdb520997ea58
Jacques Thurling <jthurling78@gmail.com>
2026-03-26 14:21:30 +0200

    new download command

---
+++ Commands/DownloadCommand.cs
@@ -0,0 +1,13 @@
+﻿using DotMake.CommandLine;
+
+namespace forge.Commands;
+
+[CliCommand(
+    Name = "download",
+    Description = "Download files from the internet",
+    Parent = typeof(RootCommand)
+)]
+public class DownloadCommand
+{
+
+}
