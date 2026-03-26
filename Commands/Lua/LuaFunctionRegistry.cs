a1f2a60a1b71a033f983b8f5247c765c13d76acd
Jacques Thurling <jthurling78@gmail.com>
2026-03-26 21:46:01 +0200

    More information

---
+++ Commands/Lua/LuaFunctionRegistry.cs
@@ -0,0 +1,17 @@
+﻿using Lua;
+
+namespace forge.Commands.Lua;
+
+public abstract class LuaFunctionModule
+{
+  public abstract string ModuleName { get; }
+  public abstract void RegisterFunctions(Dictionary<LuaValue, LuaValue> table); 
+}
+
+// TODO: Come back and implement this 
+public class CoreFunctionModule : LuaFunctionModule
+{
+    public override string ModuleName => "forge";
+    public override void RegisterFunctions(Dictionary<LuaValue, LuaValue> table)
+    {}
+}
