d55854b27631c1dc12b617a3264234f3a2a5c5ab
Jacques Thurling <jthurling78@gmail.com>
2026-04-06 20:23:41 +0200

    busy fixing testing command

--- CMakeGeneration/Sections/TestingSection.cs
+++ CMakeGeneration/Sections/TestingSection.cs
@@ -13,23 +13,36 @@ public class TestingSection : CMakeSectionBase
 
   public override string Generate(ProjectConfig config)
   {
-    return @"
+    // Collect all dependencies for linking
+    var testDeps = new List<string> { "GTest::gtest_main" };
+
+    foreach (var dep in config.Dependencies)
+    {
+      if (dep.Key != "googletest")
+      {
+        var target = string.IsNullOrEmpty(dep.Value.Target) ? dep.Key : dep.Value.Target;
+        testDeps.Add(target);
+      }
+    }
+
+    return $@"
 # --- Testing ---
 enable_testing()
-file(GLOB_RECURSE TEST_SOURCES ""${PROJECT_SOURCE_DIR}/test/*.cpp"")
-set(SOURCES_FOR_TESTS ${SOURCES})
-if(EXISTS ""${PROJECT_SOURCE_DIR}/src/main.cpp"")
-list(REMOVE_ITEM SOURCES_FOR_TESTS ""${PROJECT_SOURCE_DIR}/src/main.cpp"")
-endif()
-add_executable(run_tests ${TEST_SOURCES} ${SOURCES_FOR_TESTS})
+file(GLOB_RECURSE TEST_SOURCES ""${{PROJECT_SOURCE_DIR}}/test/*.cpp"")
+
+# Build list of sources WITHOUT src/main.cpp (to avoid multiple main)
+file(GLOB_RECURSE APP_SOURCES ""${{PROJECT_SOURCE_DIR}}/src/*.cpp"")
+list(FILTER APP_SOURCES EXCLUDE REGEX "".* main\\.cpp$"")
+
+add_executable(run_tests ${{TEST_SOURCES}} ${{APP_SOURCES}})
 target_include_directories(run_tests PRIVATE 
-    ${CMAKE_CURRENT_SOURCE_DIR}/src
-    ${CMAKE_CURRENT_BINARY_DIR}/googletest-src/googletest/include
-    ${CMAKE_CURRENT_BINARY_DIR}/googletest-src/googletest
+  ${{CMAKE_CURRENT_SOURCE_DIR}}/src
+  ${{CMAKE_CURRENT_BINARY_DIR}}/googletest-src/googletest/include
+  ${{CMAKE_CURRENT_BINARY_DIR}}/googletest-src/googletest
 )
-target_link_libraries(run_tests PUBLIC GTest::gtest_main)
+target_link_libraries(run_tests PUBLIC {string.Join(" ", testDeps)})
 include(GoogleTest)
 gtest_discover_tests(run_tests)
-      ";
+       ";
   }
 }
